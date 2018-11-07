4fd080324d13e4720206ddc6ef68981621a3a75d
BIT student <classroom@bgit.rs>
2018-11-07 14:09:52 +0100

    jquery

--- jQuery/jquery.js
+++ jQuery/jquery.js
@@ -1,4 +1,11 @@
-$(document).ready(function () {
-    console.log('hi');
+$(function () {
+
+    $('li').css('border-bottom', '1px solid black');
+    $('li').text().toUpperCase();
+    $('li.active').css('color', 'red');
+
+    var $listItems = $('li');
+    var middle = ($listItems.length - 1) / 2;//dobio je poslednji element niza ne niz
+
+    $($listItems.get(middle)).css('background-color', 'green');
 })
-$('firstLi')
